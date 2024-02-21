29161d0d91fafa3c5a625f0321419c207c27dff5
xiaomeng9597 <49544781+xiaomeng9597@users.noreply.github.com>
2024-02-21 11:35:12 +0800

    Add files via upload

---
+++ brcmfmac43430-sdio-panther-x2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# cd /lib/firmware/brcm/
+
+
+wget -O /lib/firmware/brcm/brcmfmac43430-sdio.bin https://xiaomeng9597.github.io/files/wifi/brcmfmac43430-sdio.bin
+wget -O /lib/firmware/brcm/brcmfmac43430-sdio.panther,x2.bin https://xiaomeng9597.github.io/files/wifi/brcmfmac43430-sdio.bin
+wget -O /lib/firmware/brcm/brcmfmac43430-sdio.txt https://xiaomeng9597.github.io/files/wifi/brcmfmac43430-sdio.txt
+wget -O /lib/firmware/brcm/brcmfmac43430-sdio.panther,x2.txt https://xiaomeng9597.github.io/files/wifi/brcmfmac43430-sdio.txt
+
+
+chmod 777 /lib/firmware/brcm/
+
+echo "WiFi驱动包已下载到指定目录完毕，正在重启系统中……"
+
+reboot
