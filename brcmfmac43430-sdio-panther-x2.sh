951fd3c25b5862fb2aa21613f388e7c0ae0901b9
桐话 <49544781+xiaomeng9597@users.noreply.github.com>
2024-10-07 11:02:13 +0800

    Add files via upload

--- brcmfmac43430-sdio-panther-x2.sh
+++ brcmfmac43430-sdio-panther-x2.sh
@@ -2,12 +2,12 @@
 
 # cd /lib/firmware/brcm/
 
-wget -O /lib/firmware/brcm/brcmfmac43430-sdio.bin https://qsth520.gitee.io/files/wifi/brcmfmac43430-sdio.bin
-wget -O /lib/firmware/brcm/brcmfmac43430-sdio.panther,x2.bin https://qsth520.gitee.io/files/wifi/brcmfmac43430-sdio.bin
-wget -O /lib/firmware/brcm/brcmfmac43430-sdio.clm_blob https://qsth520.gitee.io/files/wifi/brcmfmac43430-sdio.clm_blob
-wget -O /lib/firmware/brcm/brcmfmac43430-sdio.panther,x2.clm_blob https://qsth520.gitee.io/files/wifi/brcmfmac43430-sdio.clm_blob
-wget -O /lib/firmware/brcm/brcmfmac43430-sdio.txt https://qsth520.gitee.io/files/wifi/brcmfmac43430-sdio
-wget -O /lib/firmware/brcm/brcmfmac43430-sdio.panther,x2.txt https://qsth520.gitee.io/files/wifi/brcmfmac43430-sdio
+wget -O /lib/firmware/brcm/brcmfmac43430-sdio.bin https://github.com/xiaomeng9597/files/releases/download/wifi/brcmfmac43430-sdio.bin
+wget -O /lib/firmware/brcm/brcmfmac43430-sdio.panther,x2.bin https://github.com/xiaomeng9597/files/releases/download/wifi/brcmfmac43430-sdio.bin
+wget -O /lib/firmware/brcm/brcmfmac43430-sdio.clm_blob https://github.com/xiaomeng9597/files/releases/download/wifi/brcmfmac43430-sdio.clm_blob
+wget -O /lib/firmware/brcm/brcmfmac43430-sdio.panther,x2.clm_blob https://github.com/xiaomeng9597/files/releases/download/wifi/brcmfmac43430-sdio.clm_blob
+wget -O /lib/firmware/brcm/brcmfmac43430-sdio.txt https://github.com/xiaomeng9597/files/releases/download/wifi/brcmfmac43430-sdio.txt
+wget -O /lib/firmware/brcm/brcmfmac43430-sdio.panther,x2.txt https://github.com/xiaomeng9597/files/releases/download/wifi/brcmfmac43430-sdio.txt
 
 echo "WiFi驱动包已下载到指定目录完毕，正在重启系统中……"
 
